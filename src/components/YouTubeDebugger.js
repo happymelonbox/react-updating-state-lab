40d7d1aa38bc0bebf047161a41cb3aa31e3eabf0
Justin Berger Howes <howes.j.j@gmail.com>
2021-07-15 23:26:26 +1000

    change resolution working

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,40 @@
 // Code YouTubeDebugger Component Here
+import React from 'react'
+
+class YouTubeDebugger extends React.Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+          }
+    }
+
+    changeBitRate = () => {
+        this.setState()
+    }
+
+    changeResolution = () => {
+        this.setState({
+           settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    render(){
+        return (
+            <button className="bitrate" onClick={this.changeBitRate}>Change BitRate</button>,
+            <button className="resolution" onClick={this.changeResolution}></button>
+        )
+    }
+}
+
+export default YouTubeDebugger
